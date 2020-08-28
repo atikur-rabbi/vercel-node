b4ea96de21498b08f596f7bb5db9008fa42e7118
Atikur Rabbi <msawon94@gmail.com>
2020-08-29 05:54:41 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.send('Hello World!')
 })
+
+
+app.use(express.static(__dirname + '/public'));
 
 module.exports = app
